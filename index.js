aee2b87cd1b1229339c6ffc0d42ce7b0e42bd399
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-03-05 12:31:31 +0300

    test:added unit tests using Postman

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express  = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: 1,
@@ -19,16 +21,38 @@ let notes = [
   }
 ]
 
-/*app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0 
+  return maxId + 1
+}
+
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if(!body.content){
+    return response.status(404).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+  
+  
+  notes = notes.concat(note)
+  response.json(note)
 })
-*/
 
-
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes', (request, response) => { 
     const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    response.json(note)
+    notes = notes.filter(note => note.id !== id)
+    response.json(notes)
+
+    response.status(204).end();
     
        
 })
